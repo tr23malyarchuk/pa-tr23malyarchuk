609ae7dd13f7b17450509baec508c71d7edc89c5
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-10-24 22:53:29 +0300

    Returning sum of 3 elements of arctg(x) function results

---
+++ Arctangent.h
@@ -0,0 +1,10 @@
+#ifndef ARCTANGENT_H
+#define ARCTANGENT_H
+
+class Arctangent // holding a class with a public function FuncA
+{
+    public:
+        double FuncA(double x, int n);
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main() // instantiating the class and calling the function
 {
     Arctangent arctan;
     double x = 0.5;
+    int n = 3;
 
-    double result = arctan.FuncA(x);
+    double result = arctan.FuncA(x, n);
     std::cout << "Arctangent of " << x << " is approximately: " << result << std::endl;
 
     return 0;
