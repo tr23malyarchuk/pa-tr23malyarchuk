98a2085c675c4dadef01fd106e9bafdcffdcea57
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-11-17 22:14:14 +0200

    Added Makefile and configure scripts in order to build and test tarball and deb package, also included appropriate pipelines and unit test for c++ program

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "Arctangent.h"
 
-int main() // instantiating the class and calling the function
-{
+int main() {
     Arctangent arctan;
     double x = 0.5;
     int n = 10;
-
     double result = arctan.FuncA(x, n);
     std::cout << "Arctangent of " << x << " is approximately: " << result << std::endl;
-
     return 0;
 }
+
